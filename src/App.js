3363191d71e47a5b54b01d9329c926c63a49bfb4
Carl Anderson <carl.anderson@microsoft.com>
2020-09-02 00:02:36 -0500

    wip: skeletal UI interface

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+// TODO: move this to components/appHeader.js
+// TODO: replace this with a blueprint FormGroup?
+const headerPanel = (
+  <div>
+    <span class="app-title">Patient Lists Demo App</span>
+    <span class="server">
+      FHIR Server Root
+      <input></input>
+      <button>Refresh Lists</button>
+    </span>
+  </div>
+);
+
+const listsPanel = (
+  <span>Lists Panel HERE</span>
+);
+
+const patientsPanel = (
+  <div>
+    Patients Panel HERE
+  </div>
+);
+
+const attributesPanel = (
+  <div>
+    Attributes Panel HERE
+  </div>
+);
+
+const developerPanel = (
+  <div>
+    Developer Messages HERE
+  </div>
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div classname="App">
+      <div class="row">
+        <div class="top">
+          {headerPanel}
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="left">{listsPanel}</div>
+        <div class="center">{patientsPanel}</div>
+        <div class="right">{attributesPanel}</div>
+      </div>
+
+      <div class="row">
+        <div class="bottom">
+          {developerPanel}
+        </div>
+      </div>
     </div>
   );
 }
