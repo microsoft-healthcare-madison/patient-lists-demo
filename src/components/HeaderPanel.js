c4a63515bb71974fd063b569641611d666431222
Carl Anderson <carl.anderson@microsoft.com>
2020-09-02 23:49:34 -0500

    wip: refactored the class-based form component to use React.useState

--- src/components/HeaderPanel.js
+++ src/components/HeaderPanel.js
@@ -2,45 +2,38 @@ import React from 'react';
 import 'components/HeaderPanel.css'
 
 const appTitle = "Patient Lists Demo App";
-//const defaultServerRootURL = "http://localhost:8080/hapi-fhir-jpaserver/fhir/";
 
+// NICE: as the server root value is typed, evaluate it for availability / regex sanity.
+//       maybe after a few seconds time-out, enable the Discover Patient Lists button.
+//       during the timeout period, display a spinner to indicate something's happening.
 
-class ServerInputForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: props.serverRootURL,
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+function ServerInputForm(props) {
+  const [value, setValue] = React.useState(props.serverRootURL);
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
-    // TODO: visual feedback when a valid server URL is entered?  enable button?
-  }
-
-  handleSubmit(event) {
+  const discoverListsButton = (event) => {
     event.preventDefault();
-    this.props.setServerRootURL(this.state.value);
+    props.setServerRootURL(value);
   }
 
-  render() {
-    // TODO: replace with a blueprint FormGroup for prettier input.
-    // TODO: bigger button, only enabled when a valid server is entered??
-    return (
-      <form className="server" onSubmit={this.handleSubmit}>
-        <label>FHIR Server Root</label>
-        &nbsp;
-        <input
-          type="text"
-          value={this.state.value}
-          onChange={this.handleChange}
-        />
-        <input type="submit" value="Discover Patient Lists" />
-      </form>
-    );
+  const serverRootChanged = (event) => {
+    setValue(event.target.value);
+    // TODO: examine the server value as it is entered.
+    console.log('CHANGED', event.target.value);  // XXX
   }
+
+  // TODO: replace the homebrewed form with a blueprint FormGroup, maybe.
+  return (
+    <form className="server" onSubmit={discoverListsButton}>
+      <label>FHIR Server Root</label>
+      &nbsp;
+      <input
+        type="text"
+        value={value}
+        onChange={serverRootChanged}
+      />
+      <input type="submit" value="Discover Patient Lists" />
+    </form>
+  );
 }
 
 class HeaderPanel extends React.Component {
