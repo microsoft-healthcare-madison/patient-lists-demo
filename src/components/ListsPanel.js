48c788691a5247f21dab7bd99c36fbf7a31cfc06
Carl Anderson <carl.anderson@microsoft.com>
2020-09-05 00:46:53 -0500

    feat: added ability to scrape server groups for group.characteristic.value[x]

--- src/components/ListsPanel.js
+++ src/components/ListsPanel.js
@@ -1,92 +1,86 @@
 import React from 'react';
 import 'components/ListsPanel.css'
 import { Button, Checkbox, Collapse } from "@blueprintjs/core";
-import { Suggest } from "@blueprintjs/select";
+//import { Suggest } from "@blueprintjs/select";
 
+const codeSystem = "http://argonautproject.org/patient-lists/CodeSystem/characteristics";
+
+
+// Returns the non-empty resources from a bundle.
+function getResources(bundle) {
+  return bundle ? bundle.entry.map(e => e.resource).filter(Boolean) : [];
+}
+
+// Returns the patient list characteristics matching a code (ie 'at-location').
+function getCharacteristics(resources, code) {
+  const hasChars = resources.flatMap(x => x.characteristic).filter(Boolean);
+  return hasChars.filter(
+    x => x.code.coding.some(c => c.system === codeSystem && c.code === code)
+  );
+}
+
+// Returns the sorted set of Location references from lists in a bundle.
+function getRefsFrom(bundle, code) {
+  const resources = getResources(bundle);
+  const characteristics = getCharacteristics(resources, code);
+  const locations = new Set(characteristics.map(x => x.valueReference.reference));
+  return [...locations].sort();
+}
+
+// TODO: use the props.locations to populate a list of locations to select
+function LocationSelector(props) {
+  console.log(props);
+  return (
+    <>
+      {props.locations}
+    </>
+  );
+}
 
 // This component allows the user to select a 'managingEntity' from the FHIR
 // server.
 function ManagingEntity(props) {
-  React.useEffect(() => {
-    console.log('ManagingEntity: I need to update');
-  }, [props.serverRootURL]);  // XXX
-  const [isOpen, setIsOpen] = React.useState(true);
+  const [isVisible, setIsVisible] = React.useState(false);
+  // TODO: for the filters, there needs to be a state and a setter for each.
+  // TODO: create a React.useState for the selectors in the calling function, passing them through props
+  // TODO: find a suitable blueprint object that can render a list given state and setters  MultiSelect, maybe?
   return (
-    <div>
-    <Button onClick={() => { setIsOpen(!isOpen); console.log('HI'); }}>Managing Entity</Button>
-    <Collapse
-      isOpen={isOpen}
-      keepChildrenMounted="true"
-    >
-      <table border="1"><tbody>
-        <tr><td>{locations}</td></tr>
-        <tr><td>{practitioners}</td></tr>
-        <tr><td>{organizations}</td></tr>
-        <tr><td>{careTeams}</td></tr>
-      </tbody></table>
-    </Collapse>
-    </div>
+    <>
+      <Button
+        onClick={ () => { setIsVisible(!isVisible); } }
+      >Managing Entity</Button>
+      <Collapse
+        isOpen={isVisible}
+        keepChildrenMounted="true"
+      >
+        <>
+          <Button>Organization</Button>
+          <Button>Practitioner</Button>
+        </>
+      </Collapse>
+    </>
   );
 }
 
-// TODO: return a blueprint MultiSelect here
-function ListList(props) {
-  return (
-    <>
-      <h4>Lists</h4>
-
-    </>
-  )
+function getSelectedLists(bundle, selections) {
+  return [];
 }
 
-// TODO: get rid of these selectors - they should be dynamically populated from the server when a discovery is requested.
-const locations = (
-  <div className="bp3-select .modifier">
-    <select defaultValue={'DEFAULT'}>
-      <option value="DEFAULT">Locations...</option>
-      <option value="1">One</option>
-      <option value="2">Two</option>
-      <option value="3">Three</option>
-      <option value="4">Four</option>
-    </select>
-  </div>
-);
-
-const practitioners = (
-  <div className="bp3-select .modifier">
-    <select defaultValue={'DEFAULT'}>
-      <option value="DEFAULT">Practitioners...</option>
-      <option value="1">One</option>
-      <option value="2">Two</option>
-      <option value="3">Three</option>
-      <option value="4">Four</option>
-    </select>
-  </div>
-);
-
-const organizations = (
-  <div className="bp3-select .modifier">
-    <select defaultValue="DEFAULT">
-      <option value="DEFAULT">Organizations...</option>
-      <option value="1">One</option>
-      <option value="2">Two</option>
-      <option value="3">Three</option>
-      <option value="4">Four</option>
-    </select>
-  </div>
-);
-
-const careTeams = (
-  <div className="bp3-select .modifier">
-    <select defaultValue={"DEFAULT"}>
-      <option value="DEFAULT">Care Teams...</option>
-      <option value="1">One</option>
-      <option value="2">Two</option>
-      <option value="3">Three</option>
-      <option value="4">Four</option>
-    </select>
-  </div>
-);
+// TODO: populate this based on the properties selected
+// TODO: define the parameters in a function comment
+function ListSelector(props) {
+  return (
+    <table>
+      <tbody>
+        <tr><td><Checkbox>List 1</Checkbox></td></tr>
+        <tr><td><Checkbox>List 2</Checkbox></td></tr>
+        <tr><td><Checkbox>List 3</Checkbox></td></tr>
+        <tr><td><Checkbox>List 4</Checkbox></td></tr>
+        <tr><td><Checkbox>List 5</Checkbox></td></tr>
+      </tbody>
+    </table>
+  );
+}
 
 class ListsPanel extends React.Component {
   constructor(props) {
@@ -98,11 +92,12 @@ class ListsPanel extends React.Component {
   }
 
   // TODO: modify the query to include ManagingEntity attributes (and anything else)
+  // TODO: modify this to drain all the Groups if the results are paginated.
   refreshGroups() {
     fetch(this.props.serverRootURL + '/Group')
       .then(response => response.json())
       .then(bundle => this.setState({ groups: bundle}))
-      .then(() => { console.log(this.state.groups); })  // XXX
+      .then(() => { console.log("REFRESHED", this.state.groups); })  // XXX
   }
 
   componentDidMount() {
@@ -110,28 +105,32 @@ class ListsPanel extends React.Component {
   }
 
   // TODO: componentize the remaining UI objects (Filters, Lists, Header)
-  // TODO: replace the Filters with text input boxes temporarily
-  // TODO: attempt to scrape the server, pulling all Locations to populate the filter
-  // TODO: query the server to get all the Lists, populating the checklist component (using the filters to select lists).
-  // TODO: implement the Resolve Patients button.
-  // TODO: incorporate the care team by provider taxonomy (maybe???) - http://hl7.org/fhir/us/core/STU3.1.1/ValueSet-us-core-careteam-provider-roles.html
-  // TODO: beautify the header and Resolve button.
+  // TODO: replace this panel with a blueprint component
   render() {
+    const bundle = this.state.groups;
+    const locations = getRefsFrom(bundle, 'at-location');
+    const orgs = getRefsFrom(bundle, 'attributed-to-organization');
+    const docs = getRefsFrom(bundle, 'attributed-to-practitioner');
+    const careTeams = getRefsFrom(bundle, 'attributed-to-careteam');
+    // TODO: drop the this.state.groups bundle (since it might be big)?
+    // TODO: get the component selections, saving them into an object.
+    const selections = {};
+
+    // TODO: turn this into a NavBar: https://blueprintjs.com/docs/#core/components/navbar followed by a scrollable ListSelector
     return (
       <>
-        <span>Patient Lists</span>
-        <button>Display Patients</button>
+        <div>Patient Lists</div>
 
-        <ManagingEntity serverRootURL={this.props.serverRootURL} />
-
-        <h4>Lists</h4>
-        <table><tbody>
-          <tr><td><Checkbox>List 1</Checkbox></td></tr>
-          <tr><td><Checkbox>List 2</Checkbox></td></tr>
-          <tr><td><Checkbox>List 3</Checkbox></td></tr>
-          <tr><td><Checkbox>List 4</Checkbox></td></tr>
-          <tr><td><Checkbox>List 5</Checkbox></td></tr>
-          </tbody></table>
+        <LocationSelector
+          locations={locations}
+        />
+        <ManagingEntity
+          organizations={orgs}
+          practitioners={docs}
+        />
+        <ListSelector
+          lists={getSelectedLists(bundle, selections)}
+        />
       </>
     );
   }
