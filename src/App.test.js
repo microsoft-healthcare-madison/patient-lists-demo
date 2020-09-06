5c4dc03d0e754cd2212727043ca8e72f045fc145
Carl Anderson <carl.anderson@microsoft.com>
2020-09-06 00:03:12 -0500

    wip: updated tests to pass from CRA boilerplate

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('has title', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = getByText(/Patient Lists Demo App/i);
+  expect(titleElement).toBeInTheDocument();
 });
