9bec28503af8394b28719cc12e75a7bf9e122b4c
Carl Anderson <carl.anderson@microsoft.com>
2020-09-07 23:15:14 -0500

    fix: enable the messages to be scrolled up and down, and the panel hidden

--- src/components/DeveloperPanel.js
+++ src/components/DeveloperPanel.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import 'components/DeveloperPanel.css'
+import { Button, Callout, Collapse } from "@blueprintjs/core";
 
-// TODO: enable this panel to be shown or hidden by clicking a button.
-// TODO: enable this panel to be resized.
+// NICE: enable this panel to be resized up and down.
 
-class DeveloperPanel extends React.Component {
-  render() {
-    let messages = this.props.developerMessages || <pre key="0">None</pre>;
-    return (
-      <>
-        <span>Developer Messages</span>
-        <button>Hide</button><br></br>
-        {messages}
-      </>
-    );
-  }
+export default function DeveloperPanel(props) {
+  let messages = props.developerMessages || <pre key="0">None</pre>;
+  const [isHidden, setIsHidden] = React.useState(true);
+
+  return (
+    <>
+      <Button
+        onClick={() => {setIsHidden(!isHidden)}}
+        style={{zIndex: 1, position: 'relative'}}
+      >
+        {isHidden ? "Show" : "Hide"} Developer Messages
+      </Button>
+      <Collapse isOpen={!isHidden}>
+        <Callout title="Developer Messages (Most Recent At Top)">
+          <div style={{overflowY: 'scroll'}}>
+            {messages}
+          </div>
+        </Callout>
+      </Collapse>
+    </>
+  );
 }
-
-export default DeveloperPanel;
