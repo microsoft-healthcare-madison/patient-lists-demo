9b7329bc0ada9c91ef8383bd0ab762418f5cb24c
Carl Anderson <carl.anderson@microsoft.com>
2020-09-09 18:44:41 -0500

    Updates

---
+++ precache-manifest.e2ed16175ba01ac76aef348c429de258.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65dd7da4b3f3b74d2bb7163b9252d5ed",
+    "url": "/patient-lists-demo/index.html"
+  },
+  {
+    "revision": "9e42974066006ccc5117",
+    "url": "/patient-lists-demo/static/css/2.7348cf09.chunk.css"
+  },
+  {
+    "revision": "9af64212adb2846a31fe",
+    "url": "/patient-lists-demo/static/css/main.85aa9ca0.chunk.css"
+  },
+  {
+    "revision": "9e42974066006ccc5117",
+    "url": "/patient-lists-demo/static/js/2.b36b079f.chunk.js"
+  },
+  {
+    "revision": "9944a45902f915d9aae4db0c09e83634",
+    "url": "/patient-lists-demo/static/js/2.b36b079f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9af64212adb2846a31fe",
+    "url": "/patient-lists-demo/static/js/main.92f6242e.chunk.js"
+  },
+  {
+    "revision": "d2154f1fd97dfce7fd95",
+    "url": "/patient-lists-demo/static/js/runtime-main.21981bba.js"
+  },
+  {
+    "revision": "05f1cdadfe476395f60e233b15c22155",
+    "url": "/patient-lists-demo/static/media/icons-16.05f1cdad.eot"
+  },
+  {
+    "revision": "3c1c220e7a18286503fb431c7a7fe183",
+    "url": "/patient-lists-demo/static/media/icons-16.3c1c220e.woff"
+  },
+  {
+    "revision": "3cde8748332d1de6b1ae1c2dc5850754",
+    "url": "/patient-lists-demo/static/media/icons-16.3cde8748.ttf"
+  },
+  {
+    "revision": "0a5c76518a68c185baa2c6744456918c",
+    "url": "/patient-lists-demo/static/media/icons-20.0a5c7651.eot"
+  },
+  {
+    "revision": "51ec31f302d0072808e1f83f85fea4cd",
+    "url": "/patient-lists-demo/static/media/icons-20.51ec31f3.ttf"
+  },
+  {
+    "revision": "cef8cdbb9d0ba82e6e19fb0eeba2ac3d",
+    "url": "/patient-lists-demo/static/media/icons-20.cef8cdbb.woff"
+  }
+]);
